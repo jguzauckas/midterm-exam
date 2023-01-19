57d6fe2438016fb3964813ceb3f44183f820fb57
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2023-01-19 13:20:47 -0500

    Added exam files and updated README

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 For this project, complete the exercises sketched out in the exam.py file.
 
-When done correctly, your program should have the same output as the output.txt file, with the exception of lines with \*\*\* to denote random number generation affecting the output.
+When done correctly, your program should have the same output as the output.txt file, with the exception of lines with \*\*\* to denote random number generation or custom user inputs affecting the output.
 
 When done, commit and push your progress so that it is visible to me.
---
+++ exam.py
@@ -0,0 +1,71 @@
+# Create three variables named "int1", "float1", and "string1", and
+# assign them the values 16, 43.25, and "Hello, World!" respectively.
+
+
+# Print the following sentences using the variables above:
+# "I am 16 years old."
+# "I have $43.25 in my wallet."
+# "Hello, World!"
+
+
+# Make a variable named "user_int" and save a user input integer value
+# to it, with the prompt "Enter your age: "
+
+
+# Make a variable named "user_friend_int" and save a user input integer
+# value to it, with the prompt "Enter your friend's age: "
+
+
+# Dynamically print a statement about the comparison of the user's age and
+# their friend's age. Remember that there are 3 options for this comparison.
+
+
+# Create a variable named "test_string" and save the following string to it:
+# "the quick brown fox jumps over the lazy dog".
+
+
+# Print out the results of the following about test_string in nicely formatted
+# sentences:
+# - The character at index 15.
+# - The string from the beginning to index 7.
+# - The string from index 17 to the end.
+# - The number of times the character "a" appears in the string.
+
+
+# Create a list named "random_list" and save 10 random numbers between 0
+# and 1 to it.
+
+
+# Find and print out the maximum value of the list. Limit the output to
+# 2 decimal places.
+
+
+# Create a function that calculates and returns the range from a list of
+# numbers (range is the maximum value minus the minimum value). It should
+# not print the value, just return it.
+
+
+# Call the range function on "random_list" and print the output limited to
+# 3 decimal places.
+
+
+# Create a "dog" class. It should have an initializer that takes in variables
+# "name" (string), "breed" (string), "age" (integer), and "sex" (string).
+# Make a string method that will return "name is a sex age years old breed."
+# Make name and age properties with getter and setter methods using decorators.
+# Make a good_bad method that will return "He is a good boy" or "She is a good
+# girl" based on their sex value. Make an "age" method that will increase the
+# age by 1 and return the new value.
+
+
+# Create a dog object assigned to a variable "my_dog". If you can't think of
+# a dog, use Jonathan the Husky from UConn: Jonathan the 14th, Husky, 9, Male.
+
+
+# Print my_dog
+
+
+# Print the result of the good_bad method from my_dog.
+
+
+# Update the age of my_dog and print how old they will be next year:
